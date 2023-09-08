0bd913116b7158030e5bafd62f2cf92aef50150f
abdiko12 <abdissageda@gmail.com>
2023-09-08 11:52:39 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	char i;
 
-	fir (i = 'a' ; i <= 'z' ; i++)
+	for (i = 'a' ; i <= 'z' ; i++)
 		if (i != 'q' && i != 'e')
 			putchar(i);
 	putchar('\n');
-	return (0)
+	return (0);
 }
