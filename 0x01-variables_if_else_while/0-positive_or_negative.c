643a0c98cfc45beb5cefa0d550715101fd99d36d
abdiko12 <abdissageda@gmail.com>
2023-09-08 09:50:16 -0700

    chmod u+x 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+#include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
 	int n;
